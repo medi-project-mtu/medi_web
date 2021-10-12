0a8657eaa6a8b750eaadd00100861fbedcbfcf8f
Romain ubuntu laptop <romain.clemencon@mycit.ie>
2021-10-12 15:45:23 +0100

    Fixed the look of the form to match prototype.

--- src/Components/Login/loginForm.js
+++ src/Components/Login/loginForm.js
@@ -3,26 +3,24 @@ import React, { Component } from 'react'
 export default class LoginForm extends Component {
     render(){
         return (
-                <div className="rightSide col-sm-6 d-flex align-items-center justify-content-center p-0 m-0">
+                <div className="bg-login col-md-6 d-flex align-items-center justify-content-center p-0 m-0">
                     <form className="loginForm ">
-                        <h3>Sign In</h3>
-                        <div className="form-group">
-                            <label>Email address</label>
-                            <input type="email" className="form-control" placeholder="Enter email" />
+                        <h3 className="text-white">Sign In</h3>
+                        <div className="form-group pt-3">
+                            <input type="email" className="form-control" placeholder="Email address" />
                         </div>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input type="password" className="form-control" placeholder="Enter password" />
+                        <div className="form-group pt-3">
+                            <input type="password" className="form-control" placeholder="Password"/>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group py-3">
                             <div className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                                <label className="custom-control-label text-white ps-1" htmlFor="customCheck1">Remember me</label>
                             </div>
-                        </div>
-                        <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                        <p className="forgot-password text-right">
-                            Forgot <a href="/login">password?</a>
+                        </div>  
+                        <button type="submit" className="btn btn-danger btn-block">Submit</button>
+                        <p className="forgot-password text-right pt-3">
+                            <a href="/login" className="text-decoration-none text-danger">Forgot password?</a>
                         </p>
                     </form>
                 </div>
