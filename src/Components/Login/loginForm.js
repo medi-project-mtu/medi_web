ddd57a03b9dbb9e462cdcb85ae2e508e73d16d0d
Romain Clemencon <romain.clemencon@mycit.ie>
2021-10-12 23:45:33 +0100

    Linked FireBase API to the App and added login with Google too.

--- src/Components/Login/loginForm.js
+++ src/Components/Login/loginForm.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { auth, signInWithEmailAndPassword, signInWithGoogle } from "../Firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-
+import GoogleGLogo from '../../Assets/Common/Google__G__Logo.svg'
 
 function LoginForm() {
     const [email, setEmail] = useState("");
@@ -18,38 +18,47 @@ function LoginForm() {
         }, [user, loading]
     );
     return (
-            <div className="bg-login col-md-6 d-flex align-items-center justify-content-center p-0 m-0">
-                <form className="loginForm ">
-                    <h3 className="text-white">Sign In</h3>
-                    <div className="form-group pt-3">
-                        <input 
-                        type="email" 
-                        className="form-control" 
-                        name="email" 
-                        
-                        placeholder="Email address"
-                        value={email} />
+        <div className="bg-login col-md-6 d-flex align-items-center justify-content-center p-0 m-0">
+            <form className="loginForm ">
+                <h3 className="text-white">Sign In</h3>
+                <div className="form-group pt-3">
+                    <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email address"
+                    value={email} />
+                </div>
+                <div className="form-group pt-3">
+                    <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    value={password}/>
+                </div>
+                <div className="form-group py-3">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label text-white ps-1" htmlFor="customCheck1">Remember me</label>
                     </div>
-                    <div className="form-group pt-3">
-                        <input 
-                        type="password" 
-                        className="form-control" 
-                        name="password" 
-                        
-                        placeholder="Password"
-                        value={password}/>
-                    </div>
-                    <div className="form-group py-3">
-                        <div className="custom-control custom-checkbox">
-                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                            <label className="custom-control-label text-white ps-1" htmlFor="customCheck1">Remember me</label>
-                        </div>
-                    </div>  
-                    <button type="submit" className="btn btn-danger btn-block">Log In</button>
-                    <p className="forgot-password text-right pt-3">
-                        <a href="/login" className="text-decoration-none text-danger">Forgot password?</a>
-                    </p>
-                </form>
-            </div>
+                </div>
+                <div className="row p-0 m-0">
+                    <button
+                        className="col-3 btn btn-danger btn-block"
+                        onClick={() => signInWithEmailAndPassword(email, password)}
+                    >Log In</button>
+                    <h5 className="col-4 text-white mb-0 p-0 pt-2 text-center">Or use</h5>
+                    <img src={GoogleGLogo} alt="logo" className="col-2 btn logo-google" onClick={signInWithGoogle}/>
+                </div>
+
+                <p className="forgot-password text-right pt-3">
+                    <a href="/login" className="text-decoration-none text-danger">Forgot password?</a>
+                </p>
+            </form>
+        </div>
     )
 }
+export default LoginForm;
--- src/Components/Login/logo.js
+++ src/Components/Login/logo.js
@@ -7,7 +7,7 @@ export default class Logo extends Component {
         return(
                 <div className="bg-logo text-center col-md-6 leftSide p-0 m-0">
                     <div className="d-flex align-items-center justify-content-center h-100">
-                        <img src={logo} alt="logo" className="flex"/>
+                        <img src={logo} alt="logo" className="flex logo-medi"/>
                     </div>
                 </div>
         )
