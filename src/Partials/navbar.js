ff09da9cb6d691475871561d5a0ae61b9678c47e
Romain Clemencon <romain.clemencon@mycit.ie>
2021-10-27 21:51:12 +0100

    Quick update to fix redirection in navbar.js

--- src/Partials/navbar.js
+++ src/Partials/navbar.js
@@ -10,18 +10,18 @@ export default function navbar({name}) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="#"><img src={Logo} alt="Logo of mediweb" className="navbar-logo"></img></Link>
+                <Link className="navbar-brand" to="/dashboard"><img src={Logo} alt="Logo of mediweb" className="navbar-logo"></img></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav d-flex align-items-center">
-                        <Link className="nav-link active" aria-current="page" to="#"><img src={dood} alt="avatar" className="avatar-pic"></img></Link>
-                        <Link className="nav-link active" to="#"><h3>{name}</h3></Link>
+                        <Link className="nav-link active" aria-current="page" to="/dashboard"><img src={dood} alt="avatar" className="avatar-pic"></img></Link>
+                        <Link className="nav-link active" to="/dashboard"><h3>{name}</h3></Link>
                         <button type="button" className="btn-light logout-button" onClick={logout}>Log out</button>
                     </div>
                 </div>
-                <Link className="navbar-brand" to="#"><img src={dashboardLogo} alt="Logo of mediweb" className="dashboard-logo"></img></Link>
+                <Link className="navbar-brand" to="/dashboard"><img src={dashboardLogo} alt="Logo of mediweb" className="dashboard-logo"></img></Link>
             </div>
         </nav>
     )
