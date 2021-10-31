097ad6f0d4b0774242e9a1d2eaa5cea98189e411
yiming <y.tan@mycit.ie>
2021-10-31 20:32:57 +0000

    changed the styling of the patient profile card

--- src/Components/Dashboard/Profile/card.js
+++ src/Components/Dashboard/Profile/card.js
@@ -1,80 +1,94 @@
-import React from 'react'
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useParams
-  } from "react-router-dom";
-import { Redirect, useHistory } from 'react-router'
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+import { Redirect, useHistory } from "react-router";
 
-import { fetchInsurance } from '../../Firebase';
-import dood from '../../../Assets/Common/dood.png'
+import { fetchInsurance } from "../../Firebase";
+import dood from "../../../Assets/Common/dood.png";
 
-const Card = ({data, insurance}) => {
-    let { patientId } = useParams();
-    const history = useHistory();
+const Card = ({ data, insurance }) => {
+  let { patientId } = useParams();
+  const history = useHistory();
 
-    if (data[patientId] === undefined){
-        return <Redirect to="/*"/>
-    }
+  if (data[patientId] === undefined) {
+    return <Redirect to="/*" />;
+  }
 
-    const patient = data[patientId].val()    
+  const patient = data[patientId].val();
 
-    const patientInsurance = insurance.filter(obj =>{return obj.key === patient.insuranceId})
-    const insuranceName = patientInsurance[0].val().name
+  const patientInsurance = insurance.filter((obj) => {
+    return obj.key === patient.insuranceId;
+  });
+  const insuranceName = patientInsurance[0].val().name;
 
-    return (
-        <div className="patient-details">
-            <div class="d-flex justify-content-center">
-                <div className="row">
-                    <div className="col py-4">
-                        <h3 className="details-heading">Patient Details</h3>
-                    </div>
-                </div>
+  return (
+    <div className="card patient-details">
+      <h3 className="card-header">Patient Profile</h3>
+
+      <div className="d-flex justify-content-center card-body">
+        <div className="row">
+          <div className="col-4">
+            <div>
+              <label className="detail-field">Name:</label>
             </div>
-
-            <div className="d-flex justify-content-center">
-                <div className="row">
-
-                    <div className="col-3 ps-5">
-                        <img src={dood} alt="replace me" className="profile-pic"></img>
-                    </div>
-
-                    <div className="col-3">
-                        <label className="detail-field">Patient Name: {patient.name}</label>
-                    </div>
-
-                    <div className="col-3">
-                        <label className="detail-field">Date of Birth: {patient.dob}</label>
-                    </div>
-
-                    <div className="col-3">
-                        <label className="detail-field">Gender: {patient.gender}</label>
-                    </div>
-
-
-                    <div className="col-3 ps-5">
-                        <label className="detail-field"></label>
-                    </div>
-
-                    <div className="col-3">
-                        <label className="detail-field">Height: {patient.height}</label>
-                    </div>
-
-                    <div className="col-3">
-                        <label className="detail-field">Weight: {patient.weight}</label>
-                    </div>
-
-                    <div className="col-3">
-                        <label className="detail-field">Insurance Provider: {insuranceName}</label>
-                    </div>
-                    
-                </div>
+            <div>
+              <label className="detail-data">{patient.name}</label>
             </div>
-                
+          </div>
+
+          <div className="col-4">
+            <div>
+              <label className="detail-field">Date of Birth:</label>
+            </div>
+            <div>
+              <label className="detail-data">{patient.dob}</label>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div>
+              <label className="detail-field">Gender:</label>
+            </div>
+            <div>
+              <label className="detail-data">{patient.gender}</label>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div>
+              <label className="detail-field">Height:</label>
+            </div>
+            <div>
+              <label className="detail-data">{patient.height}cm</label>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div>
+              <label className="detail-field">Weight:</label>
+            </div>
+            <div>
+              <label className="detail-data">{patient.weight}kg</label>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div>
+              <label className="detail-field">Insurance Provider:</label>
+            </div>
+            <div>
+              <label className="detail-data">{insuranceName}</label>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Card;
